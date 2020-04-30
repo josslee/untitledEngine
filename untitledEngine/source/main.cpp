d03f68116ef631eddb094db6426ecd70e5d463a5
Emmanuel Sogelola <esogelola@gmail.com>
2020-04-30 19:20:27 -0400

    Updated Window Title

--- untitledEngine/source/main.cpp
+++ untitledEngine/source/main.cpp
@@ -27,7 +27,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "UntitledEngine", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
